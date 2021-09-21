a3f83f3dcd044ed44211d6e80f85c86c6fcdbf91
Anthony-S58 <tonysim01@hotmail.com>
2021-09-21 11:39:49 +0200

    newVersion

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Portfolio-V2</title>
+</head>
+<body>
+    <header id="barre">
+        
+            <div id="name"><a href="#home"><h1><span>A</span>nthony <span>S</span>imonneau</h1></a></div>
+            <div id="menu">
+                <ul>
+                    <li><a href="#about"  class="linkburger" data-after="A Propos">A Propos</a></li>
+                    <li><a href="#projets" class="linkburger" data-after="Projets">Mes Projets</a></li>
+                    <li><a href="#parcours" class="linkburger" data-after="Parcours">Mon Parcours</a></li>
+                    <li><a href="#contact" class="linkburger" data-after="Contact">Contact</a></li>
+                </ul>
+            </div>
+        
+    </header>
+    <section id="home">
+    <div id="me">
+          <h1>Bonjour,</h1>
+          <h1>Je suis</h1>
+          <h1 class="typing">Anthony SIMONNEAU</h1>
+          <br />
+          <h2>Développeur web et web mobile</h2>
+          <br />
+          <a href="#" type="button" id="btn">Portfolio</a>
+            <div id="social_link">
+                <a href="https://www.facebook.com" target="_blank"
+                ><img src="IMG/facebook-circular-logo.png" height="50px" width="50px"/></a>
+
+                <a href="https://www.linkedin.fr" target="_blank"
+                ><img src="IMG/linkedin.png" height="50px" width="50px"/></a>
+
+                <a href="https://github.com/Anthony-S58" target="_blank"
+                ><img src="IMG/github.png" height="50px" width="50px"/></a>
+
+                <a href="https://api.slack.com/" target="_blank"
+                ><img src="IMG/slack.png" height="50px" width="50px"/></a>
+
+            </div>
+    </div>
+
+    </section>
+
+    <section id="about">
+        <h1>A Propos</h1>
+    </section>
+
+    <section id="projets">
+        <h1>Mes Projets</h1>
+    </section>
+
+    <section id="parcours">
+        <h1>Mon parcours</h1>
+
+        <div id="employeurs">
+            <div id="societe">
+                <div id="logosociete"><img src="IMG/logoacs.jpg" alt=""></div>
+                <div id="titresociete">Developpeur web et web mobile</div>
+                <div id="periodeemploi">De mars 2021 à décembre 2021</div>
+                <div id="descriptionemploi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            voluptas aspernatur, nostrum veritatis quasi sequi totam tempore
+            obcaecati ex eveniet recusandae, ea aperiam soluta temporibus.</div>
+            </div>
+            <div id="societe">
+                <div id="logosociete"><img src="IMG/logomgen.jpg" alt=""></div>
+                <div id="titresociete">Conseiller en santé et prévoyance</div>
+                <div id="periodeemploi">De mars 2019 à septembre 2019</div>
+                <div id="descriptionemploi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            voluptas aspernatur, nostrum veritatis quasi sequi totam tempore
+            obcaecati ex eveniet recusandae, ea aperiam soluta temporibus.</div>
+            </div>
+            <div id="societe">
+                <div id="logosociete"><img src="IMG/logoarmatis.jpg" alt=""></div>
+                <div id="titresociete">Téléconseiller CAF</div>
+                <div id="periodeemploi">test</div>
+                <div id="descriptionemploi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            voluptas aspernatur, nostrum veritatis quasi sequi totam tempore
+            obcaecati ex eveniet recusandae, ea aperiam soluta temporibus.</div>
+            </div>
+            <div id="societe">
+                <div id="logosociete"><img src="IMG/logoasetys.jpg" alt=""></div>
+                <div id="titresociete">Gestionnaire en assurances</div>
+                <div id="periodeemploi">test</div>
+                <div id="descriptionemploi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            voluptas aspernatur, nostrum veritatis quasi sequi totam tempore
+            obcaecati ex eveniet recusandae, ea aperiam soluta temporibus.</div>
+            </div>
+            <div id="societe">
+                <div id="logosociete"><img src="IMG/logocarepolis.jpg" alt=""></div>
+                <div id="titresociete">Préparateur automobile</div>
+                <div id="periodeemploi">test</div>
+                <div id="descriptionemploi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            voluptas aspernatur, nostrum veritatis quasi sequi totam tempore
+            obcaecati ex eveniet recusandae, ea aperiam soluta temporibus.</div>
+            </div>
+            <div id="societe">
+                <div id="logosociete"><img src="" alt=""></div>
+                <div id="titresociete">Téléconseiller</div>
+                <div id="periodeemploi">test</div>
+                <div id="descriptionemploi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            voluptas aspernatur, nostrum veritatis quasi sequi totam tempore
+            obcaecati ex eveniet recusandae, ea aperiam soluta temporibus.</div>
+            </div>
+            <div id="societe">
+                <div id="logosociete"><img src="IMG/logotheatre.jpg" alt=""></div>
+                <div id="titresociete">Hôte d'accueil</div>
+                <div id="periodeemploi">test</div>
+                <div id="descriptionemploi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            voluptas aspernatur, nostrum veritatis quasi sequi totam tempore
+            obcaecati ex eveniet recusandae, ea aperiam soluta temporibus.</div>
+            </div>
+        </div>
+    </section>
+
+    <section id="contact">
+        <h1>Contact</h1>
+
+        <div id="formulaire">
+            <form action="">
+                <input type="text" placeholder="Votre nom">
+                <input type="text" placeholder="Votre mail">
+                <textarea name="" id="" cols="30" rows="10" placeholder="Votre message"></textarea>
+                <button type="submit">Envoyer</button>
+            </form>
+        </div>
+    </section>
+
+    <footer>
+    <div id="footer">
+      <p>Copyright &copy; 2021 Anthony SIMONNEAU. All Rights Reserved</p>
+    </div>
+    </footer>
+
+
+<script src="js/main.js"></script>
+</body>
+</html>
